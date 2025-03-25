bdb397e93fc5afa4ef56e1dd1aa0bb1e4e8d10cf
shaiksafroz12345 <shaikafroz2k4@gmail.com>
2025-03-25 10:52:37 +0530

    first commit

---
+++ src/components/App1.jsx
@@ -0,0 +1,8 @@
+export default function App1() {
+    return (
+      <div>
+        <h3>This is App1</h3>
+        <p style={{backgroundColor:'pink',color:'blue'}}>This is a paragraph</p>
+      </div>
+    );
+  }
