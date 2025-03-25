5cc0955e3760a98041ad7697e0842355c02e6fa0
shaiksafroz12345 <shaikafroz2k4@gmail.com>
2025-03-25 15:23:12 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5"
 import App6 from "./components/App6";
+import App7 from "./components/App7";
+import App8 from "./components/App8";
+
 function App() {
   return (
     <BrowserRouter>
@@ -16,7 +19,10 @@ function App() {
         <Link to="app3">App3</Link>-
         <Link to="app4">App4</Link>-
         <Link to="app5">App5</Link>-
-        <Link to="app6">App6</Link>
+        <Link to="app6">App6</Link>-
+        <Link to="app7">App7</Link>-
+        <Link to="app8">App8</Link>
+
       </div>
       <hr></hr>
       <div>
@@ -28,6 +34,9 @@ function App() {
           <Route path="app4" element={<App4 />} />
           <Route path="app5" element={<App5 />} />
           <Route path="app6" element={<App6 />} />
+          <Route path="app7" element={<App7 />} />
+          <Route path="app8" element={<App8 />} />
+
         </Routes>
       </div>
     </BrowserRouter>
